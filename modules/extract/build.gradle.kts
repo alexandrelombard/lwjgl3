b7c96e0c6ebd1efb6c7cfec0728f9385dae4b02c
Alexandre Lombard <alexandre.lombard@live.fr>
2020-05-02 18:19:25 +0200

    Adding gradle tasks for clean

---
+++ modules/extract/build.gradle.kts
@@ -0,0 +1,7 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
---
+++ modules/generator/build.gradle.kts
@@ -0,0 +1,8 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+plugins {
+    java
+    kotlin("jvm") version "1.3.61"
+}
